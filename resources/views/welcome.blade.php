932e1c544890227244426584d57d17d2396ebf55
mikana0918 <shogo.kusuhara@gmail.com>
2021-01-17 17:30:35 +0900

    不要なコメントを削除

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,12 +5,11 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <!-- ページタイトル -->
         <title>サンプルアプリ</title>
     </head>
     <body>
         <div id="app">
-            <!-- デフォルトだとこの中ではvue.jsが有効 -->
-            <!-- example-component はLaravelに入っているサンプルのコンポーネント -->
             <router-view></router-view>
         </div>
     </body>
